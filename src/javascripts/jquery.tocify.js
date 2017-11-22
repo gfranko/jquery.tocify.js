b59f560d326cdae02c2a519e1c9e8cdb87b7fad7
Matthias Redmann <susikju@gmail.com>
2017-11-22 20:14:42 +0100

    always "scrollTo" elem if find a valid hash

--- src/javascripts/jquery.tocify.js
+++ src/javascripts/jquery.tocify.js
@@ -318,12 +318,12 @@
                 elem.addClass(self.focusClass);
 
                 // If the showAndHide option is true
-                if(self.options.showAndHide) {
+                // if(self.options.showAndHide) {
 
                     // Triggers the click event on the currently focused TOC item
                     elem.click();
 
-                }
+                // }
 
             }
 
